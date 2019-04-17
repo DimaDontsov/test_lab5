52d0e580547ed4b70bada9b46d811892af6fdaed
DimaDontsov <Dimayahoo@mail.ru>
2019-04-17 10:18:17 +0400

    Добавлено считывание элементов массива в vector_mul

--- vector_mul.cpp
+++ vector_mul.cpp
@@ -8,11 +8,12 @@ int main()
 {
 	int size;
 	cin>>size;
-	vector<int>elems(size);
 	int mul_elems = 0;
 	for (int i = 0; i < size; i++)
 	{
-		mul_elems *= elems[i];
+		int elem = 0;
+		cin>>elem;
+		mul_elems *= elem;
 	}
 	cout<<mul_elems<<endl;
 	return 0;
