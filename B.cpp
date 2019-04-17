992f51e33228ebcaffc1ce6b5a2b68e4a13d018e
DimaDontsov <Dimayahoo@mail.ru>
2019-04-17 09:49:28 +0400

    Реализовано сложение элементов массива в B.cpp

--- B.cpp
+++ B.cpp
@@ -6,6 +6,14 @@ using namespace std;
 
 int main()
 {
-	
+	int size;
+	cin>>size;
+	vector<int>elems(size);
+	int sum_elems = 0;
+	for (int i = 0; i < size; i++)
+	{
+		sum_elems += elems[i];
+	}
+	cout<<sum_elems<<endl;
 	return 0;
 }
