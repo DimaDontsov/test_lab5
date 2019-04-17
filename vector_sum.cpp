e9186fef5819c1f3c5fd5e2cd6f567f4ab3467fc
DimaDontsov <Dimayahoo@mail.ru>
2019-04-17 12:16:25 +0400

    update vector_sum

--- vector_sum.cpp
+++ vector_sum.cpp
@@ -7,13 +7,13 @@ using namespace std;
 int main()
 {
 	int size;
-	cin>>size;
-	vector<int>elems(size);
+	cin >> size;
+	vector < int > elems(size);
 	int sum_elems = 0;
 	for (int i = 0; i < size; i++)
 	{
 		sum_elems += elems[i];
 	}
-	cout<<sum_elems<<endl;
+	cout << sum_elems << endl;
 	return 0;
 }
